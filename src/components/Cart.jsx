cbba637b7569ce13cf2623923f96d3e743466ce7
OmarBknd <omar.bknd@gmail.com>
2024-12-10 23:45:51 +0100

    added cart page

---
+++ src/components/Cart.jsx
@@ -0,0 +1,36 @@
+import { useOutletContext } from 'react-router-dom';
+
+export default function Cart() {
+  const { cart } = useOutletContext();
+
+console.log(cart);
+
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      
+        <ul>
+        <div className="flex flex-col gap-5">
+        {cart.map((product) => (
+          <div
+            className=" w-40 h-60  flex flex-col items-center justify-around border rounded-md  "
+            key={product.id}
+          >
+            <p>{product.title} </p>
+            <span>${product.price}</span>
+            <span>
+             <p>{product.quantity}</p>
+              <img className="w-10 h-10" src={product.image} alt="" />
+            </span>
+       
+         
+          </div>
+        ))}
+        
+      </div>
+        </ul>
+      
+    </div>
+  );
+}
